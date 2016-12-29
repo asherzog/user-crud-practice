1d87c7d5cc2db03cd8a5a691f79538842b09d7ca
Andy Herzog <andyherzog@Andys-MacBook-Pro.local>
2016-12-29 12:40:14 -0700

    created loggin route and set cookie

--- auth/index.js
+++ auth/index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+
+
 const router = express.Router();
 
 const User = require('../db/query');
@@ -26,15 +29,60 @@ router.post('/signup', (req, res, next) => {
       .getOneByEmail(req.body.email)
       .then(user => {
         if (!user) {
-          res.json({
-            message: '👍'
-          });
+          bcrypt.hash(req.body.password, 8)
+            .then(hash => {
+              const user = {
+                email: req.body.email,
+                password: hash,
+                name: req.body.name,
+                age: req.body.age
+              };
+              User
+                .create(user)
+                .then(id => {
+                  res.json({
+                    id,
+                    message: '👍'
+                  });
+                });
+            });
         } else {
           next(new Error('Email in use'));
         }
       });
   } else {
     next(new Error('Invalid user'));
+  }
+});
+
+
+router.post('/login', (req, res, next) => {
+  if(validUser(req.body)) {
+    User
+      .getOneByEmail(req.body.email)
+      .then(user => {
+        if (user) {
+          bcrypt.compare(req.body.password, user.password)
+            .then(result => {
+              if (result) {
+                res.cookie('user_id', user.id, {
+                  httpOnly: true,
+                  signed: true,
+                  secure: req.app.get('env') != 'development'
+                });
+                res.json({
+                  message: 'Logged in!'
+                });
+              } else {
+                next(new Error('Invalid Password'));
+              }
+            });
+        } else {
+          next(new Error('Invalid login'));
+        }
+      });
+  } else {
+    next(new Error('Invalid login'));
   }
 });
 
